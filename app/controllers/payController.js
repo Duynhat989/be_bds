0b8f81dd91ae9628b421b61e4112162bf2f49716
duynhat989 <vietduy989kc@gmail.com>
2024-11-24 23:53:16 +0700

    update new 11

--- app/controllers/payController.js
+++ app/controllers/payController.js
@@ -69,11 +69,11 @@ exports.pays = async (req, res) => {
 };
 exports.findById = async (req, res) => {
     try {
-        const { page = 1, limit = 10, status_pay, startday, endday, user_id } = req.query;
+        const { page = 1, limit = 10, pay_status, startday, endday, user_id } = req.query;
         const offset = parseInt(page - 1) * parseInt(limit)
         let condition = {}
-        if (status_pay || status_pay == 0) {
-            condition.status_pay = status_pay
+        if (pay_status || pay_status == 0) {
+            condition.status_pay = pay_status
         }
         if (user_id) {
             condition.user_id = user_id
