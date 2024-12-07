fcd39de5e390dad99082cb0824ade2ffc28c6d00
duynhat989 <vietduy989kc@gmail.com>
2024-12-07 16:02:28 +0700

    update time check email

--- payment/index.js
+++ payment/index.js
@@ -4,7 +4,7 @@ const payLoop = async () => {
     const PASSWORD = process.env.PAYMENT_PAW
     // ---------------------
     let NUM_EMAILS = 20;
-    let LIMIT_DELAY = 100;
+    let LIMIT_DELAY = 150;
     connect.startChecking(EMAIL,PASSWORD,NUM_EMAILS,LIMIT_DELAY)
 }
 module.exports = {
